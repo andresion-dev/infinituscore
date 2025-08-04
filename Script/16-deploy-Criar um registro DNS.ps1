ae480adb74dd678f0024727a58d55a3899afb321
Andre Sion <andresion@outlook.com.br>
2025-08-03 22:13:03 -0300

    Update 16-deploy-Criar um registro DNS.ps1

--- Script/16-deploy-Criar um registro DNS.ps1
+++ Script/16-deploy-Criar um registro DNS.ps1
@@ -1,7 +1,17 @@
-$resourceGroup = "rg-ntier"
-$zoneName = "comunidadecloudexpert.ga"
-$ipaddress  = "<ip publico do aplication Gateway>"
+param (
+  [string]$ip,
+  [string]$resourceGroup,
+  [string]$zoneName,
+  [string]$recordSetName = "www"  # valor padrão se não for especificado
+)
 
+Write-Host "Registrando IP $ip no DNS zone '$zoneName' no resource group '$resourceGroup' para o record set '$recordSetName'"
+
+az network dns record-set a add-record `
+  --resource-group $resourceGroup `
+  --zone-name $zoneName `
+  --record-set-name $recordSetName `
+  --ipv4-address $ip
 
 az network dns record-set a add-record -g $resourceGroup -z $zoneName -n www -a $ipaddress
  
