1dbd93b336b9ef2dc1aab4d4792c0945affc110b
Andre Sion <andresion@outlook.com.br>
2025-08-04 17:01:39 -0300

    Update 02-deploy-Criar-vnet-e-a-subnet-do-Application-Gateway.sh

--- Script/02-deploy-Criar-vnet-e-a-subnet-do-Application-Gateway.sh
+++ Script/02-deploy-Criar-vnet-e-a-subnet-do-Application-Gateway.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
-resourceGroup = "rg-ntier"
-location = "eastus"
-vNetName = "vnet-ntier"
+resourceGroup="rg-ntier"
+location="eastus"
+vNetName="vnet-ntier"
 addressPrefixVNet="10.5.0.0/16"
-subnetNameApg = "subnet-apg"
-subnetPrefixApg = "10.5.0.0/24"
+subnetNameApg="subnet-apg"
+subnetPrefixApg="10.5.0.0/24"
 az network vnet create --resource-group $resourceGroup `
