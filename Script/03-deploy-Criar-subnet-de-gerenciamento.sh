1f885497ec950cabde1d3ec67abbb8e13c94db4a
Andre Sion <andresion@outlook.com.br>
2025-08-04 12:57:07 -0300

    Update 03-deploy-Criar-subnet-de-gerenciamento.sh

--- Script/03-deploy-Criar-subnet-de-gerenciamento.sh
+++ Script/03-deploy-Criar-subnet-de-gerenciamento.sh
@@ -1,6 +1,11 @@
-$resourceGroup = "rg-ntier"
-$vNetName = "vnet-ntier"
-$subnetBastionName = "AzureBastionSubnet"
-$subnetPrefixBastion = "10.5.254.0/26"
+#!/bin/bash
+resourceGroup="rg-ntier"
+vNetName="vnet-ntier"
+subnetBastionName="AzureBastionSubnet"
+subnetPrefixBastion="10.5.254.0/26"
 echo "Creating subnet BastionHost"
-az network vnet subnet create --address-prefix $subnetPrefixBastion --name $subnetBastionName --resource-group $resourceGroup --vnet-name $vNetName
+az network vnet subnet create \
+  --address-prefix "$subnetPrefixBastion" \
+  --name "$subnetBastionName" \
+  --resource-group "$resourceGroup" \
+  --vnet-name "$vNetName"
