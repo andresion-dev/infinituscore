0980a4b66b01c397e316faec62516230550b2324
Andre Sion <andresion@outlook.com.br>
2025-08-04 13:02:16 -0300

    Update 07-deploy-Criar-subnet-Entra-ID.sh

--- Script/07-deploy-Criar-subnet-Entra-ID.sh
+++ Script/07-deploy-Criar-subnet-Entra-ID.sh
@@ -1,6 +1,11 @@
-$resourceGroup = "rg-ntier"
-$vNetName = "vnet-ntier"
-$subnetADDSName = "subnet-adds"
-$subnetPrefixADDS = "10.5.4.0/24"
+#!/bin/bash
+resourceGroup="rg-ntier"
+vNetName="vnet-ntier"
+subnetADDSName="subnet-adds"
+subnetPrefixADDS="10.5.4.0/24"
 echo "Creating subnet AD"
-az network vnet subnet create --address-prefix $subnetPrefixADDS --name $subnetADDSName --resource-group $resourceGroup --vnet-name $vNetName
+az network vnet subnet create \
+  --address-prefix "$subnetPrefixADDS" \
+  --name "$subnetADDSName" \
+  --resource-group "$resourceGroup" \
+  --vnet-name $vNetName
