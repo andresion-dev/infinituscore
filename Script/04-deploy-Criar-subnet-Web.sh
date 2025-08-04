9eb883ad3361e901803125996b23ae69b05bc09b
Andre Sion <andresion@outlook.com.br>
2025-08-04 12:29:43 -0300

    Update 04-deploy-Criar-subnet-Web.sh

--- Script/04-deploy-Criar-subnet-Web.sh
+++ Script/04-deploy-Criar-subnet-Web.sh
@@ -1,8 +1,6 @@
-
 $resourceGroup = "rg-ntier"
 $vNetName = "vnet-ntier"
 $subnetWebName = "subnet-web"
 $subnetPrefixWeb = "10.5.1.0/24"
-
 echo "Creating subnet Web"
 az network vnet subnet create --address-prefix $subnetPrefixWeb --name $subnetWebName --resource-group $resourceGroup --vnet-name $vNetName
