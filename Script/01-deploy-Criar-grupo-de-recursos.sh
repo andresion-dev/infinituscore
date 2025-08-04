dd08f0b810500eff07907f628365954168bc39af
Andre Sion <andresion@outlook.com.br>
2025-08-04 15:56:35 -0300

    Update 01-deploy-Criar-grupo-de-recursos.sh

--- Script/01-deploy-Criar-grupo-de-recursos.sh
+++ Script/01-deploy-Criar-grupo-de-recursos.sh
@@ -1,5 +1,6 @@
 
- $name = "rg-ntier"
- $location = "eastus"
+name="rg-ntier"
+location="eastus"
+echo "Creating resource group: $name in $location"
+az group create --name "$name" --location "$location"
 
- az group create --name $name --location $location
