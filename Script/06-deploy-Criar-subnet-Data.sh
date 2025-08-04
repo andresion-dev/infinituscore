bc93f16893d757004dc11fe6923dd51c3c38a29c
Andre Sion <andresion@outlook.com.br>
2025-08-04 13:01:07 -0300

    Update 06-deploy-Criar-subnet-Data.sh

--- Script/06-deploy-Criar-subnet-Data.sh
+++ Script/06-deploy-Criar-subnet-Data.sh
@@ -1,6 +1,11 @@
-$resourceGroup = "rg-ntier"
-$vNetName = "vnet-ntier"
-$subnetDataName = "subnet-data"
-$subnetPrefixData = "10.5.3.0/24"
+#!/bin/bash
+resourceGroup="rg-ntier"
+vNetName="vnet-ntier"
+subnetDataName="subnet-data"
+subnetPrefixData="10.5.3.0/24"
 echo "Creating subnet data"
-az network vnet subnet create --address-prefix $subnetPrefixData --name $subnetDataName --resource-group $resourceGroup --vnet-name $vNetName
+az network vnet subnet create \
+  --address-prefix "$subnetPrefixData" \
+  --name "$subnetDataName" \
+  --resource-group "$resourceGroup" \
+  --vnet-name "$vNetName"
