141bf83fc2bc8438581652c69469637285cc8d96
Andre Sion <andresion@outlook.com.br>
2025-08-03 22:55:09 -0300

    Update 03-deploy-Criar subnet de gerenciamento.ps1

--- Script/03-deploy-Criar subnet de gerenciamento.ps1
+++ Script/03-deploy-Criar subnet de gerenciamento.ps1
@@ -1,7 +1,7 @@
 $resourceGroup = "rg-ntier"
 $vNetName = "vnet-ntier"
 $subnetBastionName = "AzureBastionSubnet"
-$subnetPrefixBastion = "10.5.254.0/27"
+$subnetPrefixBastion = "10.5.254.0/26"
 
 echo "Creating subnet BastionHost"
 az network vnet subnet create --address-prefix $subnetPrefixBastion --name $subnetBastionName --resource-group $resourceGroup --vnet-name $vNetName
